5e6ca257ea7ff702a625fd68a2fca94d16a5fe09
Yuan Liang <yuan@yuans-MacBook-Pro.local>
2016-03-27 13:44:15 -0400

    other commit

---
+++ quick_sort.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<cstddef>
+#include<vector>
+namespace quick_sort_helper{
+   auto swap = [](auto& x,auto& y){ 
+      auto tmp = std::move(x);
+      x = std::move(y); 
+      y = std::move(tmp);
+   };
+   template <typename iterator>
+      auto partition(iterator begin, iterator end){
+	 auto pivot = end;
+	 auto r = begin;
+	 for (auto i = begin;i != (end+1); i++){
+	    if (*i < *pivot ){
+	       swap(*i,*r);
+	       r++;
+	    }
+	 }
+	 swap(*pivot,*r);
+	 return r;
+      }
+   template<typename iterator>
+      void quick_sort_helper(iterator begin,iterator end){
+	 if (end > begin){
+	    auto p = partition<iterator>(begin,end);
+	    quick_sort_helper(begin, --p);
+	    quick_sort_helper(++p, end);
+	 };    
+      }
+
+}
+template<typename Container>
+void quick_sort(Container& arr){
+   quick_sort_helper::quick_sort_helper(arr.begin(),arr.end()-1);
+}
+
+
+int main(){
+   std::vector<int> v{10,5,4,7,2};
+   quick_sort(v);
+   std::cout<<"array is ";
+      for(auto i:v)
+	 std::cout<<i<<" ";
+      std::cout<<"\n";
+}
