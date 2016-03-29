31f22a8afeba51df7b1d0cfe47971c27d47fee70
Yuan Liang <yuan@yuans-MacBook-Pro.local>
2016-03-29 15:31:45 -0400

    update

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<list>
+#include<algorithm>
 namespace{
    auto swap = [](auto& a,auto& b){
       auto tmp = std::move(a);
@@ -15,31 +16,27 @@ void insertion_sort(bidirectional_iterator begin,
    for (++cur; cur != end; ++cur){
       auto pos = cur;
       --pos;
-      while (*cur < *pos  ){
+      while (*cur < *pos && pos != begin  ){
 	 --pos;
-	 if (pos == begin)
-	    break;
       }
       ++pos;
-      for (auto i = cur; i != pos; --i){
-	 auto tmp = i;
-	 swap(*i,*--tmp);
-      }
-     if (begin == --pos){
-      if (*begin > *++pos)
-	 swap(*begin,*pos);
-     }
+      auto tmp = cur;
+      std::rotate(pos,cur,++tmp);
+      if (begin == --pos && *begin > *++pos)
+	 swap(*begin,*pos);  
    }
-
 }
 
 int main(){
-   std::list<int> v{8,10,101,1,5,3,9,6,7};
+   std::vector<int> v;
+   for (auto i = 0; i < 100;i++)
+      v.push_back(i);
+   random_shuffle(v.begin(),v.end());
    std::cout<<"before sorting\n";
    for (auto i:v)
       std::cout<<i<<" ";
    std::cout<<"\n";
-   insertion_sort(v.begin(),v.end());
+   insertion_sort(v.rbegin(),v.rend());
    for (auto i:v)
       std::cout<<i<<" ";
 }
