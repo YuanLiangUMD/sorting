4188fc5e8a78f0c5c1ba5f5f280446bb77cc49c1
Yuan Liang <yuan@yuans-MacBook-Pro.local>
2016-04-21 08:48:01 -0400

    ~

--- test_quick_sort.cpp
+++ test_quick_sort.cpp
@@ -4,10 +4,15 @@
 #include"./object_t.hpp"
 #include"./quick_sort.hpp"
 int main(){
-   std::vector<object_t> v{80,30,40,70,10,20,60,50};
+   std::vector<object_t> v;
+   v.reserve(1000);
+   for(auto i = 0; i != 100; i++)
+      v.emplace_back(i);
+
+   std::random_shuffle(v.begin(),v.end());
    quick_sort<>(v.begin(),v.end());
    for(auto& i: v)
-     std::cout<<i<<" "; 
+     std::cout<<i.ID()<<" "; 
    auto total_comparisons = std::accumulate(v.cbegin(),v.cend(),0,
 	 [](const auto& a,const auto& b) {return a+b.comparison();});
    std::cout<<"total comparisons is "<<total_comparisons<<std::endl;
